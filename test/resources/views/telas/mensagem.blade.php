e8dd8cc4e32ef40d88ee7dc6398a44ae8dd491f0
DanielleMariano <daniellebmariano@gmail.com>
2017-06-05 15:24:06 -0300

    TEST

--- test/resources/views/telas/mensagem.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.principal')
-
-@section('conteudo')
-
-<h1>Recurso indisponível no momento</h1>
-<h3>Desculpe o transtorno, estamos trabalhando para que a plataforma fique completa.</h3>
-
-@endif
-@stop
